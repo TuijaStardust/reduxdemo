0be70ba4460903c848b2ea56a131ddea2e929f33
TuijaStardust <yllapito@tuijapispa.fi>
2019-10-11 09:16:33 +0300

    Updates

---
+++ precache-manifest.3091f204b18b86ec4752f101ef646c28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a562e754253036b36a70933a25241f",
+    "url": "/reduxdemo/index.html"
+  },
+  {
+    "revision": "caf95a0f08c6f1f2d264",
+    "url": "/reduxdemo/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "1d1baaf0b3d710b3e87e",
+    "url": "/reduxdemo/static/js/2.e43b78ad.chunk.js"
+  },
+  {
+    "revision": "caf95a0f08c6f1f2d264",
+    "url": "/reduxdemo/static/js/main.4018a906.chunk.js"
+  },
+  {
+    "revision": "d9f9d1578978ecbb5751",
+    "url": "/reduxdemo/static/js/runtime-main.ac15b0fb.js"
+  }
+]);
